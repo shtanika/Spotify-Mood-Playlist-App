1999b2a3895bb1d61066ad21e7624e913b2eea7f
Tanika Shamalia <tanika.shamalia27@myhunter.cuny.edu>
2025-03-03 00:53:40 -0500

    updated

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -26,7 +26,7 @@ const Navbar = () => {
             onClick={() => signIn("spotify")}
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
           >
-            <BsSpotify size={20} /> Sign in with Spotify
+            <BsSpotify size={20} /> Sign in
           </button>
         </div>
       </div>
