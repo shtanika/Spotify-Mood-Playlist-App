c30a1c4324aa547df594771b91ca4e1a1dc243cb
Joshua Linang <jlinang06@gmail.com>
2025-03-05 16:57:00 -0500

    resources listed and other comments

--- frontend/src/app/api/auth/[...nextauth]/route.js
+++ frontend/src/app/api/auth/[...nextauth]/route.js
@@ -1,4 +1,15 @@
-// authorize a user
+
+/* Resources
+    Spotify Web API - Authorization Code Flow
+    https://developer.spotify.com/documentation/web-api/tutorials/code-flow
+
+    NextAuth.js - Getting Started and SpotifyProvider
+    https://next-auth.js.org/getting-started/example
+    https://next-auth.js.org/providers/spotify#example
+
+    Helpful blog, using nextauth approach
+    https://dev.to/matdweb/how-to-authenticate-a-spotify-user-in-nextjs-14-using-nextauth-5f6i
+*/
 
 import NextAuth from 'next-auth';
 import SpotifyProvider from 'next-auth/providers/spotify';
@@ -19,6 +30,7 @@ export const handler = NextAuth({
     ],
     // gives authorize code and access token
     callbacks: {
+        // uses JWT, json web token, for role verification and user permissons
         async jwt({ token, account }) {
             if (account) {
                 token.accessToken = account.accessToken;
