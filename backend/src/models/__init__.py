042040102f81a431b6e3f0766660c608e12ea213
Tanika Shamalia <tanika.shamalia27@myhunter.cuny.edu>
2025-04-07 12:01:08 -0400

    redundant files

--- backend/src/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from src.extensions import db
-from src.models.user import User
-from src.models.playlist import Playlist
-from src.models.prompt import Prompt
-from src.models.playlist_track import PlaylistTrack
-
-__all__ = ['User', 'Playlist', 'Prompt', 'PlaylistTrack']
